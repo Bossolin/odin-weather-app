c087f6be5984f70d0b0832d348c12c60313ef8d7
Bossolin <vladyslav.okipnyak@icloud.com>
2021-12-27 13:58:20 +0100

    added input field and refactored processData

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ let dataObj = {};
 
 const processData = (data) => {
   dataObj = {
-    weather: {
-      icon: data.weather[0].main,
-      description: data.weather[0].description,
-    },
-    wind: data.wind.speed,
-    temperature: {
-      temp: data.main.temp,
-      feels: data.main.feels_like,
-      humidity: data.main.humidity,
-    },
+    clouds: data.clouds,
+    main: data.main,
+    name: data.name,
+    rain: data.rain,
+    snow: data.snow,
+    weather: data.weather[0],
+    wind: data.wind,
   };
 };
 
@@ -23,12 +20,28 @@ const getWeatherData = async (location) => {
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=7e290a13a31e4917e8dde47f392e9439`
     );
     const data = await request.json();
-    // dataObj = { ...data };
     processData(data);
     console.log(dataObj);
   } catch (err) {
+    // Add error message when no city found
     console.log("ERROR =>", err);
   }
 };
 
-getWeatherData("Tromso");
+const submitCity = (e) => {
+  e.preventDefault();
+  const city = e.path[1][0].value;
+  getWeatherData(city);
+};
+
+const formTag = (() => {
+  const form = document.createElement("form");
+  const input = document.createElement("input");
+  const btn = document.createElement("button");
+  btn.innerText = "»";
+  btn.addEventListener("click", submitCity);
+  form.appendChild(input, btn);
+  return form;
+})();
+
+document.body.appendChild(formTag);
